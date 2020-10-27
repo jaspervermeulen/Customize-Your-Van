178556ab06597bc05ccfed812b5b046eb593bbe3
jaspervermeulen <jaspervermeulen.be@icloud.com>
2020-10-27 20:54:26 +0100

    massive big boy cleanup of javascript

--- script.js
+++ script.js
@@ -1,217 +1,29 @@
-let valueBed;
-let valueKitchen;
-let valueBoard;
-let valueFlag;
-let valueSolar;
-let valueLamps;
-let valueStickers;
-let valueWindows;
-let valueRug;
-let valueCabinet;
+const docSelector = value => document.querySelector(value);
 
-let buttonFlag = document.querySelector('.button_flag');
-let buttonBed = document.querySelector('.button_bed');
-let buttonKitchen = document.querySelector('.button_kitchen');
-let buttonBoard = document.querySelector('.button_board');
-let buttonSolar = document.querySelector('.button_solar');
-let buttonLamps = document.querySelector('.button_lamps');
-let buttonStickers = document.querySelector('.button_stickers');
-let buttonWindows = document.querySelector('.button_windows');
-let buttonRug = document.querySelector('.button_rug');
-let buttonCabinet = document.querySelector('.button_cabinet');
-let buttonClearAll = document.querySelector('.button_clearAll');
-
-let cabinet = document.querySelector('.van_cabinet');
-let rug = document.querySelector('.van_rug');
-let windows = document.querySelector('.van_window');
-let stickers = document.querySelector('.van_stickers');
-let kitchen = document.querySelector('.van_kitchen');
-let board = document.querySelector('.van_board');
-let flag = document.querySelector('.van_flag');
-let solar = document.querySelector('.van_solar');
-let lamp = document.querySelector('.van_lamps');
-let bed = document.querySelector('.van_bed');
-
-
-
-
-const displayBed = () => {
-  valueBed = !valueBed;
-  if(valueBed === true){
-    bed.style.display = "block";
-    buttonBed.style.backgroundColor = "#a3a496";
-  }else if(valueBed === false){
-    bed.style.display = "none";
-    buttonBed.style.backgroundColor = "#ecede0";
+const toggleDisplay = (e) => {
+  if(docSelector(`.${e.target.attributes[1].value}`).className.split(" ")[1] === "true"){
+    docSelector(`.${e.target.attributes[1].value}`).style.display = "block";
+    e.currentTarget.style.backgroundColor = "#a3a496";
+    docSelector(`.${e.target.attributes[1].value}`).classList.remove("true");
+    docSelector(`.${e.target.attributes[1].value}`).classList.add("false");
+  }else if(docSelector(`.${e.target.attributes[1].value}`).className.split(" ")[1] === "false"){
+    docSelector(`.${e.target.attributes[1].value}`).style.display = "none";
+    e.currentTarget.style.backgroundColor = "#ecede0";
+    docSelector(`.${e.target.attributes[1].value}`).classList.remove("false");
+    docSelector(`.${e.target.attributes[1].value}`).classList.add("true");
   }
 }
 
-const displayKitchen = () => {
-  valueKitchen = !valueKitchen;
-  if(valueKitchen === true){
-    kitchen.style.display = "block";
-    buttonKitchen.style.backgroundColor = "#a3a496";
-  }else if(valueKitchen === false){
-    kitchen.style.display = "none";
-    buttonKitchen.style.backgroundColor = "#ecede0";
+const refreshPage = () => location.reload();
 
-  }
-}
-
-const displayBoard = () => {
-  valueBoard = !valueBoard;
-  if(valueBoard === true){
-    board.style.display = "block";
-    buttonBoard.style.backgroundColor = "#a3a496";
-
-  }else if(valueBoard === false){
-    board.style.display = "none";
-    buttonBoard.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const displayFlag = () => {
-  valueFlag = !valueFlag;
-  if(valueFlag === true){
-    flag.style.display = "block";
-    buttonFlag.style.backgroundColor = "#a3a496";
-
-  }else if(valueFlag === false){
-    flag.style.display = "none";
-    buttonFlag.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const displaySolar = () => {
-  valueSolar = !valueSolar;
-  if(valueSolar === true){
-    solar.style.display = "block";
-    buttonSolar.style.backgroundColor = "#a3a496";
-
-  }else if(valueSolar === false){
-    solar.style.display = "none";
-    buttonSolar.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const displayLamps = () => {
-  valueLamps = !valueLamps;
-  if(valueLamps === true){
-    lamp.style.display = "block";
-    buttonLamps.style.backgroundColor = "#a3a496";
-
-  }else if(valueLamps === false){
-    lamp.style.display = "none";
-    buttonLamps.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const displayStickers = () => {
-  valueStickers = !valueStickers;
-  if(valueStickers === true){
-    stickers.style.display = "block";
-    buttonStickers.style.backgroundColor = "#a3a496";
-
-  }else if(valueStickers === false){
-    stickers.style.display = "none";
-    buttonStickers.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const displayWindows = () => {
-  valueWindows = !valueWindows;
-  if(valueWindows === true){
-    windows.style.display = "block";
-    buttonWindows.style.backgroundColor = "#a3a496";
-
-  }else if(valueWindows === false){
-    windows.style.display = "none";
-    buttonWindows.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const displayRug = () => {
-  valueRug = !valueRug;
-  if(valueRug === true){
-    rug.style.display = "block";
-    buttonRug.style.backgroundColor = "#a3a496";
-
-  }else if(valueRug === false){
-    rug.style.display = "none";
-    buttonRug.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const displayCabinet = () => {
-  valueCabinet = !valueCabinet;
-  if(valueCabinet === true){
-    cabinet.style.display = "block";
-    buttonCabinet.style.backgroundColor = "#a3a496";
-
-  }else if(valueCabinet === false){
-    cabinet.style.display = "none";
-    buttonCabinet.style.backgroundColor = "#ecede0";
-
-  }
-}
-
-const clearAll = () => {
-  valueCabinet = !valueCabinet;
-  cabinet.style.display = "none";
-  buttonCabinet.style.backgroundColor = "#ecede0";
-
-  valueRug = !valueRug;
-  rug.style.display = "none";
-  buttonRug.style.backgroundColor = "#ecede0";
-
-  valueWindows = !valueWindows;
-  windows.style.display = "none";
-  buttonWindows.style.backgroundColor = "#ecede0";
-
-  valueStickers = !valueStickers;
-  stickers.style.display = "none";
-  buttonStickers.style.backgroundColor = "#ecede0";
-
-  valueLamps = !valueLamps;
-  lamp.style.display = "none";
-  buttonLamps.style.backgroundColor = "#ecede0";
-
-  valueSolar = !valueSolar;
-  solar.style.display = "none";
-  buttonSolar.style.backgroundColor = "#ecede0";
-
-  valueFlag = !valueFlag;
-  flag.style.display = "none";
-  buttonFlag.style.backgroundColor = "#ecede0";
-
-  valueBoard = !valueBoard;
-  board.style.display = "none";
-  buttonBoard.style.backgroundColor = "#ecede0";
-
-  valueKitchen = !valueKitchen;
-  kitchen.style.display = "none";
-  buttonKitchen.style.backgroundColor = "#ecede0";
-
-  valueBed = !valueBed;
-  bed.style.display = "none";
-  buttonBed.style.backgroundColor = "#ecede0";
-}
-
-buttonClearAll.addEventListener("click", clearAll);
-buttonSolar.addEventListener("click", displaySolar);
-buttonFlag.addEventListener("click", displayFlag);
-buttonBoard.addEventListener("click", displayBoard);
-buttonKitchen.addEventListener("click", displayKitchen);
-buttonBed.addEventListener("click", displayBed);
-buttonLamps.addEventListener("click", displayLamps);
-buttonStickers.addEventListener("click", displayStickers);
-buttonWindows.addEventListener("click", displayWindows);
-buttonRug.addEventListener("click", displayRug);
-buttonCabinet.addEventListener("click", displayCabinet);
+docSelector('.button_bed').addEventListener("click", toggleDisplay);
+docSelector('.button_kitchen').addEventListener("click", toggleDisplay);
+docSelector('.button_clearAll').addEventListener("click", refreshPage);
+docSelector('.button_solar').addEventListener("click", toggleDisplay);
+docSelector('.button_flag').addEventListener("click", toggleDisplay);
+docSelector('.button_board').addEventListener("click", toggleDisplay);
+docSelector('.button_lamps').addEventListener("click", toggleDisplay);
+docSelector('.button_stickers').addEventListener("click", toggleDisplay);
+docSelector('.button_windows').addEventListener("click", toggleDisplay);
+docSelector('.button_rug').addEventListener("click", toggleDisplay);
+docSelector('.button_cabinet').addEventListener("click", toggleDisplay);
