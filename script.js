5926858b3d30b4f797d0c2150a6d9a62a0b8dbbe
jaspervermeulen <jaspervermeulen.be@icloud.com>
2020-12-10 21:20:39 +0100

    restyled

--- script.js
+++ script.js
@@ -3,18 +3,36 @@ const docSelector = value => document.querySelector(value);
 const toggleDisplay = (e) => {
   if(docSelector(`.${e.target.attributes[1].value}`).className.split(" ")[1] === "true"){
     docSelector(`.${e.target.attributes[1].value}`).style.display = "block";
-    e.currentTarget.style.backgroundColor = "#a3a496";
+    e.currentTarget.style.backgroundColor = "#000000";
+    e.currentTarget.style.color = "#ffffff";
     docSelector(`.${e.target.attributes[1].value}`).classList.remove("true");
     docSelector(`.${e.target.attributes[1].value}`).classList.add("false");
   }else if(docSelector(`.${e.target.attributes[1].value}`).className.split(" ")[1] === "false"){
     docSelector(`.${e.target.attributes[1].value}`).style.display = "none";
-    e.currentTarget.style.backgroundColor = "#ecede0";
+    e.currentTarget.style.backgroundColor = "#ffffff";
+    e.currentTarget.style.color = "#000000";
     docSelector(`.${e.target.attributes[1].value}`).classList.remove("false");
     docSelector(`.${e.target.attributes[1].value}`).classList.add("true");
   }
 }
 
 const refreshPage = () => location.reload();
+
+const flip = () => {
+  const van = document.querySelectorAll('.ee');
+
+  van.forEach(item => {
+    if (item.style.transform === "scaleX(-1)") {
+      item.style.transform = "scaleX(1)";
+    } else {
+      item.style.transform = "scaleX(-1)";
+    }
+  })
+
+  
+  
+  
+}
 
 docSelector('.button_bed').addEventListener("click", toggleDisplay);
 docSelector('.button_kitchen').addEventListener("click", toggleDisplay);
@@ -27,3 +45,4 @@ docSelector('.button_stickers').addEventListener("click", toggleDisplay);
 docSelector('.button_windows').addEventListener("click", toggleDisplay);
 docSelector('.button_rug').addEventListener("click", toggleDisplay);
 docSelector('.button_cabinet').addEventListener("click", toggleDisplay);
+docSelector('.button_flip').addEventListener("click", flip);
